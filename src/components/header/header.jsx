23fd2182671b4ce9863d1f71d02b67b3779b8d69
Reinzchand <dragoriza123@gmail.com>
2023-05-18 17:06:49 +0700

    Header and Homepage Raw

---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import './header.css'
+
+export const Header = () => {
+  return (
+    <header>
+          <a href="" className="logo">MyBlog</a>
+            <nav>
+              <a href="" className="login">Login</a>
+              <a href="" className="register">Register</a>
+            </nav>
+        </header>
+  )
+}
---
+++ src/components/posts/posts.jsx
@@ -0,0 +1,19 @@
+import './posts.css'
+
+export const Posts = () => {
+  return (
+    <div className="post">
+              <div className="img">
+                <img src="https://cdn.wallpapersafari.com/43/73/N2r35D.jpg" alt="" />
+              </div>
+                <div className="text">
+                  <h2>Kitagawa Marin waifu sejuta umat</h2>
+                  <p className='info'>
+                    <a href='' className="author">Brainzu</a> 
+                    <time>2023-05-18 15:08</time>
+                  </p>
+                  <p className='summary'>Anime dengan banyak unsur kesadisan, darah, dan adegan saling bunuh biasanya cenderung dihindari oleh sebagian orang. Jenis anime ini punya penggemar tersendiri.  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis deleniti, pariatur doloribus ratione omnis voluptatibus velit delectus amet iusto debitis!</p>
+                </div>
+            </div> 
+  )
+}
